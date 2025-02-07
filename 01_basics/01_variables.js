a698e47e286f47b2c1324ec4c47aa3e7bba8c38d
Mohit Yadav <40685337+Mohit3969@users.noreply.github.com>
2025-02-07 05:38:22 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountID = 1234
+console.log(accountID)
+// accountID = 526 // const variable not allowed to change
+
+let accountName = "Mohit"
+var accountEmail = "mohit@gmail.com" 
+account_city = "Indore"
+
+/*
+Perfer not to use var because of issue in block scope and functional scope. use const or let
+*/
+console.table([accountID, accountName, accountEmail, account_city])
+
